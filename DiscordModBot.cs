cdc01f2b5ba3d65d498400cc60f64945079bf1f7
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-02-26 04:52:01 -0800

    fix message checking

--- DiscordModBot.cs
+++ DiscordModBot.cs
@@ -144,6 +144,28 @@ namespace DiscordModBot
         /// </summary>
         static void InitLoggers(DiscordBot bot)
         {
+            bot.ClientConfig.OtherMessageHandling += (message) =>
+            {
+                try
+                {
+                    // TODO: helper ping on first post (never posted on the discord guild prior to 10 minutes ago,
+                    // -> never posted in any other channel, pings a helper/dev/bot,
+                    // -> and nobody else has posted in that channel since their first post) reaction,
+                    // -> and if not in a help lobby redirect to help lobby (in same response)
+                    string authorName = NameUtilities.Username(message.Author);
+                    if (WarningUtilities.GetWarnableUser((message.Channel as SocketGuildChannel).Guild.Id, message.Author.Id).SeenUsername(authorName, out string oldName))
+                    {
+                        UserCommands.SendGenericPositiveMessageReply(message, "Rename Notice", $"Notice: User <@{message.Author.Id}> changed their base username from `{oldName}` to `{authorName}`.");
+                    }
+                    // TODO: Spam detection
+                    NameUtilities.AsciiNameRuleCheck(message as IUserMessage, message.Author as SocketGuildUser);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error while processing a message: {ex}");
+                }
+            };
             bot.Client.UserJoined += (user) =>
             {
                 if (bot.BotMonitor.ShouldStopAllLogic())
@@ -201,33 +223,6 @@ namespace DiscordModBot
                     }
                 }
                 Console.WriteLine("Failed to warn of dangerous user-join: " + user.Id + " to " + user.Guild.Id + "(" + user.Guild.Name + ")");
-                return Task.CompletedTask;
-            };
-            bot.Client.MessageReceived += (socketMessage) =>
-            {
-                try
-                {
-                    if (!(socketMessage is IUserMessage message))
-                    {
-                        return Task.CompletedTask;
-                    }
-                    // TODO: helper ping on first post (never posted on the discord guild prior to 10 minutes ago,
-                    // -> never posted in any other channel, pings a helper/dev/bot,
-                    // -> and nobody else has posted in that channel since their first post) reaction,
-                    // -> and if not in a help lobby redirect to help lobby (in same response)
-                    string authorName = NameUtilities.Username(message.Author);
-                    if (WarningUtilities.GetWarnableUser((message.Channel as SocketGuildChannel).Guild.Id, message.Author.Id).SeenUsername(authorName, out string oldName))
-                    {
-                        UserCommands.SendGenericPositiveMessageReply(message, "Rename Notice",  $"Notice: User <@{message.Author.Id}> changed their base username from `{oldName}` to `{authorName}`.");
-                    }
-                    // TODO: Spam detection
-                    NameUtilities.AsciiNameRuleCheck(message as IUserMessage, message.Author as SocketGuildUser);
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error while processing a message: {ex}");
-                }
                 return Task.CompletedTask;
             };
             bot.Client.MessageUpdated += (cache, message, channel) =>
