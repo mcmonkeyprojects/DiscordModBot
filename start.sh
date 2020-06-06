34566d35cec025336d7e1cb4adda2dd68b1823cf
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-05 21:16:02 -0700

    fix start script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull origin master
 git submodule update --init --recursive
 dotnet build DiscordModBot.sln --configuration Release -o ./bin/live_release
-screen -dmS DiscordModBot dotnet bin/live_release/DiscordModBot.dll -- $1
+screen -dmS DiscordModBot dotnet bin/live_release/DiscordModBot.dll $1
