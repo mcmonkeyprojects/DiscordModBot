8514ff72aaa5314525523eb52e5e5a5a0580a100
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-03-05 16:35:28 -0800

    fix accidental blocking of decimals in  durations

--- WarningHandlers/WarningUtilities.cs
+++ WarningHandlers/WarningUtilities.cs
@@ -96,7 +96,7 @@ namespace ModBot.WarningHandlers
         public static HashSet<string> PermanentWords = new() { "permanent", "permanently", "indefinite", "indefinitely", "forever" };
 
         /// <summary>Helper to separate digits from letters, for <see cref="ParseDuration(string, bool)"/>.</summary>
-        public static AsciiMatcher DigitMatcher = new(AsciiMatcher.Digits);
+        public static AsciiMatcher DigitMatcher = new(AsciiMatcher.Digits + ".");
 
         /// <summary>Helper to identify timespan suffix keywords, for <see cref="ParseDuration(string, bool)"/>.</summary>
         public static Dictionary<string, string> TimespanSuffixRemapper = new();
