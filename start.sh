18cbcaa530f8e938bca8ad8296d8d1a5b61be5e0
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-08 22:10:59 -0800

    cleanup/bump, and also way easier restart

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+git pull origin master
+git submodule update --init --recursive
+rm -rf ./bin/live_release_old
+mv ./bin/live_release ./bin/live_release_old
+dotnet build DiscordModBot.sln --configuration Release -o ./bin/live_release
+dotnet bin/live_release/DiscordModBot.dll $1
 
-screen -dmS DiscordModBotStart bash ./real_start.sh $1
+# Exit code 42 means restart, anything else = don't.
+if [ $? == 42 ]; then
+    source ./start.sh
+fi
